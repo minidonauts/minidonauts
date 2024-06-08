1e278dbd982540abb71274b6fb80ffa0b186197b
minidonauts <104803128+minidonauts@users.noreply.github.com>
2024-06-08 07:29:41 -0500

    simple changes

--- make-a-reservation.js
+++ make-a-reservation.js
@@ -3,10 +3,12 @@ function sendEmail() {
     var subject = 'Blast Off with Mini Donauts: Reservation Inquiry';
     var emailBody = "Hi,\n" +
         `To provide a better idea of my event, here is some extra details:\n` +
-        `\tCity/Zipcode: \n` +
-        `\tTime donuts should start to be served: \n` +
-        `\tDuration of donut production: \n` +
-        `\tEstimated number of guests: \n` +
+        `\tEvent City/Zipcode (e.g. 60174): \n` +
+        `\tEvent Date (e.g. June 16th): \n` +
+        `\tTime donuts should start to be served (e.g. 10AM): \n` +
+        `\tDuration of donut production (e.g. 1 hour): \n` +
+        `\tEstimated number of guests (e.g. 40): \n` +
+        `\tI want custom labels (e.g. N or Y, "Good luck at ABC, Norbert!"): \n` +
         `\n`;
     document.location = "mailto:" + email + "?subject=" + subject + "&body=" + encodeURIComponent(emailBody);
 }
--- navbar.js
+++ navbar.js
@@ -4,4 +4,30 @@ fetch(window.location.origin + "/navbar.html")
     })
     .then(data => {
         document.querySelector("partial-navbar").innerHTML = data;
+
+        var tabCurrentMarkets = document.querySelector('#tab-currentMarkets');
+        var tabMakeAReservation = document.querySelector('#tab-makeAReservation');
+        var tabAboutUs = document.querySelector('#tab-aboutUs');
+
+        if (window.location.pathname == "/current-markets.html") {
+            tabCurrentMarkets.classList.add('tab-selected');
+            tabCurrentMarkets.classList.remove('tab-unselected');
+        } else {
+            tabCurrentMarkets.classList.remove('tab-selected');
+            tabCurrentMarkets.classList.add('tab-unselected');
+        }
+        if (window.location.pathname == "/make-a-reservation.html") {
+            tabMakeAReservation.classList.add('tab-selected');
+            tabMakeAReservation.classList.remove('tab-unselected');
+        } else {
+            tabMakeAReservation.classList.remove('tab-selected');
+            tabMakeAReservation.classList.add('tab-unselected');
+        }
+        if (window.location.pathname == "/about-us.html") {
+            tabAboutUs.classList.add('tab-selected');
+            tabAboutUs.classList.remove('tab-unselected');
+        } else {
+            tabAboutUs.classList.remove('tab-selected');
+            tabAboutUs.classList.add('tab-unselected');
+        }
     });
